b8b3fdcfab927facb8497244e491b5a5f79c97c2
Stanko Krtalić <stanko.krtalic@gmail.com>
2016-01-24 19:33:16 +0100

    Namespace controllers

--- app/controllers/admin/applications_controller.rb
+++ app/controllers/admin/applications_controller.rb
@@ -1,62 +1,64 @@
-class ApplicationsController < AuthorizedController
-  def index
-    @applications = application_scope
-  end
+module Admin
+  class ApplicationsController < AuthorizedController
+    def index
+      @applications = application_scope
+    end
 
-  def show
-    load_application
-  end
+    def show
+      load_application
+    end
 
-  def new
-    build_application
-  end
+    def new
+      build_application
+    end
 
-  def create
-    build_application
-    save_application || render(:new)
-  end
+    def create
+      build_application
+      save_application || render(:new)
+    end
 
-  def edit
-    load_application
-  end
+    def edit
+      load_application
+    end
 
-  def update
-    load_application
-    build_application
-    save_application || render(:edit)
-  end
+    def update
+      load_application
+      build_application
+      save_application || render(:edit)
+    end
 
-  def destroy
-    load_application
-    @application.destroy
-    redirect_to [:admin, :index]
-  end
+    def destroy
+      load_application
+      @application.destroy
+      redirect_to [:admin, :index]
+    end
 
-  private
+    private
 
-  def application_params
-    return {} unless prams[:application]
-    params.require(:application).permit(
-      :title, :start_date, :end_date, :type
-    )
-  end
+    def application_params
+      return {} unless prams[:application]
+      params.require(:application).permit(
+        :title, :start_date, :end_date, :type
+      )
+    end
 
-  def save_application
-    return unless @application.save
+    def save_application
+      return unless @application.save
 
-    redirect_to [:admin, @application]
-  end
+      redirect_to [:admin, @application]
+    end
 
-  def build_application
-    @application ||= Application.new
-    @application.attributes = application_params
-  end
+    def build_application
+      @application ||= Application.new
+      @application.attributes = application_params
+    end
 
-  def load_application
-    @application = application_scope.find(params[:id])
-  end
+    def load_application
+      @application = application_scope.find(params[:id])
+    end
 
-  def application_scope
-    Application.all
+    def application_scope
+      Application.all
+    end
   end
 end
--- app/controllers/admin/seasons_controller.rb
+++ app/controllers/admin/seasons_controller.rb
@@ -1,62 +1,65 @@
-class SeasonsController < AuthorizedController
-  def index
-    @seasons = season_scope
-  end
+module Admin
+  class SeasonsController < AuthorizedController
+    def index
+      @seasons = season_scope
+    end
 
-  def show
-    load_season
-  end
+    def show
+      load_season
+    end
 
-  def new
-    build_season
-  end
+    def new
+      build_season
+    end
 
-  def create
-    build_season
-    save_season || render(:new)
-  end
+    def create
+      build_season
+      save_season || render(:new)
+    end
 
-  def edit
-    load_season
-  end
+    def edit
+      load_season
+    end
 
-  def update
-    load_season
-    build_season
-    save_season || render(:edit)
-  end
+    def update
+      load_season
+      build_season
+      save_season || render(:edit)
+    end
 
-  def destroy
-    load_season
-    @season.destroy
-    redirect_to [:admin, :index]
-  end
+    def destroy
+      load_season
+      @season.destroy
+      redirect_to [:admin, :index]
+    end
 
-  private
+    private
 
-  def season_params
-    return {} unless prams[:season]
-    params.require(:season).permit(
-      :title, :start_date, :end_date, :type
-    )
-  end
+    def season_params
+      return {} unless prams[:season]
+      params.require(:season).permit(
+        :title, :start_date, :end_date, :type
+      )
+    end
 
-  def save_season
-    return unless @season.save
+    def save_season
+      return unless @season.save
 
-    redirect_to [:admin, @season]
-  end
+      redirect_to [:admin, @season]
+    end
 
-  def build_season
-    @season ||= Season.new
-    @season.attributes = season_params
-  end
+    def build_season
+      @season ||= Season.new
+      @season.attributes = season_params
+    end
 
-  def load_season
-    @season = season_scope.find(params[:id])
-  end
+    def load_season
+      @season = season_scope.find(params[:id])
+    end
 
-  def season_scope
-    Season.all
+    def season_scope
+      Season.all
+    end
+
   end
 end
