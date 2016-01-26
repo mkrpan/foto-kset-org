5d67bc3f7bc5c668368a050a11d3aef51549447b
Stanko Krtalić <stanko.krtalic@gmail.com>
2016-01-26 15:09:59 +0100

    Configure deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,73 +1,18 @@
 require 'mina/bundler'
 require 'mina/rails'
 require 'mina/git'
-# require 'mina/rbenv'  # for rbenv support. (http://rbenv.org)
-# require 'mina/rvm'    # for rvm support. (http://rvm.io)
+require 'mina/dotenv'
 
-# Basic settings:
-#   domain       - The hostname to SSH to.
-#   deploy_to    - Path to deploy into.
-#   repository   - Git repo to clone from. (needed by mina/git)
-#   branch       - Branch name to deploy. (needed by mina/git)
-
-set :domain, 'foobar.com'
-set :deploy_to, '/var/www/foobar.com'
-set :repository, 'git://...'
+set :domain, '161.53.74.113'
+set :deploy_to, '/home/foto/foto_kset_org'
+set :repository, 'git@github.com:KSET/foto-kset-org.git'
 set :branch, 'master'
+set :user, 'foto'
 
-# For system-wide RVM install.
-#   set :rvm_path, '/usr/local/rvm/bin/rvm'
+set :shared_paths, ['log', '.env']
 
-# Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
-# They will be linked in the 'deploy:link_shared_paths' step.
-set :shared_paths, ['config/database.yml', 'config/secrets.yml', 'log']
-
-# Optional settings:
-#   set :user, 'foobar'    # Username in the server to SSH to.
-#   set :port, '30000'     # SSH port number.
-#   set :forward_agent, true     # SSH forward_agent.
-
-# This task is the environment that is loaded for most commands, such as
-# `mina deploy` or `mina rake`.
-task :environment do
-  # If you're using rbenv, use this to load the rbenv environment.
-  # Be sure to commit your .ruby-version or .rbenv-version to your repository.
-  # invoke :'rbenv:load'
-
-  # For those using RVM, use this to load an RVM version@gemset.
-  # invoke :'rvm:use[ruby-1.9.3-p125@default]'
-end
-
-# Put any custom mkdir's in here for when `mina setup` is ran.
-# For Rails apps, we'll make some of the shared paths that are shared between
-# all releases.
-task :setup => :environment do
-  queue! %[mkdir -p "#{deploy_to}/#{shared_path}/log"]
-  queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/log"]
-
-  queue! %[mkdir -p "#{deploy_to}/#{shared_path}/config"]
-  queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/config"]
-
-  queue! %[touch "#{deploy_to}/#{shared_path}/config/database.yml"]
-  queue! %[touch "#{deploy_to}/#{shared_path}/config/secrets.yml"]
-  queue  %[echo "-----> Be sure to edit '#{deploy_to}/#{shared_path}/config/database.yml' and 'secrets.yml'."]
-
-  queue %[
-    repo_host=`echo $repo | sed -e 's/.*@//g' -e 's/:.*//g'` &&
-    repo_port=`echo $repo | grep -o ':[0-9]*' | sed -e 's/://g'` &&
-    if [ -z "${repo_port}" ]; then repo_port=22; fi &&
-    ssh-keyscan -p $repo_port -H $repo_host >> ~/.ssh/known_hosts
-  ]
-end
-
-desc "Deploys the current version to the server."
-task :deploy => :environment do
-  to :before_hook do
-    # Put things to run locally before ssh
-  end
+task deploy: :stages do
   deploy do
-    # Put things that will set up an empty directory into a fully set-up
-    # instance of your project.
     invoke :'git:clone'
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
@@ -76,15 +21,37 @@ task :deploy => :environment do
     invoke :'deploy:cleanup'
 
     to :launch do
-      queue "mkdir -p #{deploy_to}/#{current_path}/tmp/"
-      queue "touch #{deploy_to}/#{current_path}/tmp/restart.txt"
+      queue! %(echo "-----> Symlink system to shared")
+      queue! "echo \"ln -nFs #{deploy_to}/shared/system #{deploy_to}/#{current_path}/public/system\""
+      queue! %(ln -nFs #{deploy_to}/shared/system #{deploy_to}/#{current_path}/public/system)
+
+      invoke :restart_application
     end
   end
 end
 
-# For help in making your deploy script, see the Mina documentation:
-#
-#  - http://nadarei.co/mina
-#  - http://nadarei.co/mina/tasks
-#  - http://nadarei.co/mina/settings
-#  - http://nadarei.co/mina/helpers
+# First ensure that the stage is selected
+task :stages do
+  # unless domain
+  #   print_error 'A server needs to be specified. e.g. production/staging'
+  #   exit
+  # end
+end
+
+# Just overwrote to execute stages first
+task setup: :stages do
+  queue! %(mkdir -p "#{deploy_to}/shared/log")
+  queue! %(chmod g+rx,u+rwx "#{deploy_to}/shared/log")
+end
+
+# Tail log from server
+task log: :stages do
+  queue "tail -f #{deploy_to}/#{current_path}/log/#{rails_env}.log"
+end
+
+# Restart application after deployment
+task :restart_application do
+  queue %(echo "-----> Restarting application")
+  queue "mkdir -p #{deploy_to}/#{current_path}/tmp/"
+  queue "touch #{deploy_to}/#{current_path}/tmp/restart.txt"
+end
