bf95ddfbdde5245f4066615579063cc41917d94a
Stanko Krtalić <stanko.krtalic@gmail.com>
2016-01-26 11:28:46 +0100

    Fix frontend errors

--- app/assets/javascripts/public.js
+++ app/assets/javascripts/public.js
@@ -7,30 +7,42 @@ $(document).ready(function() {
     $("#TecajModalButton").animate({
         left: "0vw"
     }, 200);
-  }), $("#TecajContainer").mouseleave(function() {
+  });
+
+  $("#TecajContainer").mouseleave(function() {
     $("#TecajModalButton").animate({
         left: "-100vw"
     }, 500);
-  }), $("#RadionicaContainer").mouseenter(function() {
+  });
+
+  $("#RadionicaContainer").mouseenter(function() {
     $("#RadionicaModalButton").animate({
         right: "0vw"
     }, 200);
-  }), $("#RadionicaContainer").mouseleave(function() {
+  });
+
+  $("#RadionicaContainer").mouseleave(function() {
     $("#RadionicaModalButton").animate({
         right: "-100vw"
     }, 500);
-  }), $(window).scroll(function() {
+  });
+
+  $(window).scroll(function() {
     if ($(this).scrollTop() > 500) {
       $("#BackToTop").fadeIn(400);
     }
     else {
       $("#BackToTop").fadeOut(400);
     }
-  }), $("#BackToTop").click(function() {
+  });
+
+  $("#BackToTop").click(function() {
     return $("html, body").animate({
         scrollTop: 0
     }, 1e3), !1;
-  }), $("#FollowUsFooter img").hover(function() {
+  });
+
+  $("#FollowUsFooter img").hover(function() {
     $(this).transition({
         scale: 1.5
     });
@@ -38,13 +50,17 @@ $(document).ready(function() {
     $(this).transition({
         scale: 1
     });
-  }), $(".Shortcut").click(function(o) {
+  });
+
+  $(".Shortcut").click(function(o) {
     o.preventDefault();
     var t = $(this).attr("href");
     t = $("" + t).offset().top, $("html, body").animate({
         scrollTop: t - 300
     }, 2e3);
-  }), $(".ClosePrijaveModal").click(function() {
+  });
+
+  $(".ClosePrijaveModal").click(function() {
     $("#TecajModal, #RadionicaModal").modal("hide");
   });
 
@@ -63,4 +79,6 @@ $(document).ready(function() {
   $('#SubmitInlineTecaj').click(function() {
     $('#PrijavaTecaj .workshop-form-container form').submit();
   });
+
+  $('body').scrollspy({ target: '.navbar', offset: 400 });
 });
