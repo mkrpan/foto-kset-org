f2d6c3e6aa412cad2cf08436acd816944ce4ee65
Stanko Krtalić <stanko.krtalic@gmail.com>
2016-02-11 15:16:00 +0100

    Copy Krpans changes

--- app/assets/javascripts/public.js
+++ app/assets/javascripts/public.js
@@ -4,83 +4,59 @@
 //= require jquery.cookie
 //= require phrasing
 
-$(document).ready(function() {
-  $("#TecajContainer").mouseenter(function() {
-    $("#TecajModalButton").animate({
-        left: "0vw"
-    }, 200);
+$(document).ready(function(){
+  $(window).load(function () {
+      $('#preloader').fadeOut('slow', function () { $(this).remove(); });
   });
 
-  $("#TecajContainer").mouseleave(function() {
-    $("#TecajModalButton").animate({
-        left: "-100vw"
-    }, 500);
+  $("#TecajContainer").mouseenter( function(){
+    $("#TecajModalButton").animate({left: "0vw"},200);
   });
 
-  $("#RadionicaContainer").mouseenter(function() {
-    $("#RadionicaModalButton").animate({
-        right: "0vw"
-    }, 200);
+  $("#TecajContainer").mouseleave( function(){
+    $("#TecajModalButton").animate({left: "-100vw"},500);
   });
 
-  $("#RadionicaContainer").mouseleave(function() {
-    $("#RadionicaModalButton").animate({
-        right: "-100vw"
-    }, 500);
+  $("#RadionicaContainer").mouseenter( function(){
+    $("#RadionicaModalButton").animate({right: "0vw"},200);
   });
 
-  $(window).scroll(function() {
-    if ($(this).scrollTop() > 500) {
+  $("#RadionicaContainer").mouseleave( function(){
+    $("#RadionicaModalButton").animate({right: "-100vw"},500);
+  });
+
+  $(window).scroll( function(){
+    if ( $(this).scrollTop() > 500 ) {
       $("#BackToTop").fadeIn(400);
     }
-    else {
+    else{
       $("#BackToTop").fadeOut(400);
     }
   });
 
-  $("#BackToTop").click(function() {
-    return $("html, body").animate({
-        scrollTop: 0
-    }, 1e3), !1;
+  $('#BackToTop').click(function(){
+    $("html, body").animate({ scrollTop: 0 }, 1000);
+    return false;
   });
 
-  $("#FollowUsFooter img").hover(function() {
-    $(this).transition({
-        scale: 1.5
+  $("#FollowUsFooter img").hover(
+    function(){
+      $(this).transition({scale:1.5});
+    },
+    function(){
+      $(this).transition({scale:1.0});
     });
-  }, function() {
-    $(this).transition({
-        scale: 1
-    });
+
+  $(".Shortcut").click(function(e){
+    e.preventDefault();
+    var divID = $(this).attr("href");
+    positionFromTop = $("" + divID).offset().top;
+    var offset = $(window).height() / 2 - $("" + divID).height() / 2;
+    var scrollValue = positionFromTop - offset;
+    $("html, body").animate({ scrollTop: scrollValue }, 2000);
   });
 
-  $(".Shortcut").click(function(o) {
-    o.preventDefault();
-    var t = $(this).attr("href");
-    t = $("" + t).offset().top, $("html, body").animate({
-        scrollTop: t - 300
-    }, 2e3);
-  });
-
-  $(".ClosePrijaveModal").click(function() {
+  $(".ClosePrijaveModal").click(function(){
     $("#TecajModal, #RadionicaModal").modal("hide");
   });
-
-  $('#SubmitRadionica').click(function() {
-    $('.modal-body .workshop-form-container form').submit();
-  });
-
-  $('#SubmitTecaj').click(function() {
-    $('.modal-body .course-form-container form').submit();
-  });
-
-  $('#SubmitInlineRadionica').click(function() {
-    $('#PrijavaRadionica .workshop-form-container form').submit();
-  });
-
-  $('#SubmitInlineTecaj').click(function() {
-    $('#PrijavaTecaj .course-form-container form').submit();
-  });
-
-  $('body').scrollspy({ target: '.navbar', offset: 400 });
 });
